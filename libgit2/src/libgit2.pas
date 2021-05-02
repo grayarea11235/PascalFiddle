8851ed2c2cbf51e9c26f500844679a6c555a225c
grayarea11235 <ciaran.dunn@gmail.com>
2021-05-02 15:11:07 +0100

    libgit2 added

--- libgit2/src/libgit2.pas
+++ libgit2/src/libgit2.pas
@@ -1,4 +1,22 @@
 { Libgit2 }
+unit libgit2;
+
+{$link ??}
+
+interface
+
+uses ctypes;
+
+{ int git_libgit2_init(); }
+function git_libgit2_init : cint32; cdecl; external;
+
+{ int git_libgit2_shutdown(); }
+function git_libgit2_shiutdown : cint32; cdecl; external;
+
+{ int git_repository_open(git_repository **out, const char *path); }
+{ function git_repository_open(var }
 
 
+implementation 
 
+end.
