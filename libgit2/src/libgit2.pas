bcc64d9fc5545790650edb0910bed1cc39ede704
grayarea11235 <ciaran.dunn@gmail.com>
2021-05-04 00:51:04 +0100

    libgit2

--- libgit2/src/libgit2.pas
+++ libgit2/src/libgit2.pas
@@ -12,7 +12,7 @@ uses
 function git_libgit2_init : cint32; cdecl; external;
 
 { int git_libgit2_shutdown(); }
-function git_libgit2_shiutdown : cint32; cdecl; external;
+function git_libgit2_shutdown : cint32; cdecl; external;
 
 { int git_repository_open(git_repository **out, const char *path); }
 { function git_repository_open(var }
--- libgit2/src/main.pas
+++ libgit2/src/main.pas
@@ -1,6 +1,7 @@
 program TestLibGit2; 
 
 uses
+  sysutils,
   libgit2,
   ctypes; 
 
@@ -12,5 +13,10 @@ begin
 
   WriteLn('About to call git_libgit2_init');
   res := git_libgit2_init(); 
+  WriteLn(Format('res = %d', [res]));
 
+
+  WriteLn('About to call git_libgit2_shutdown');
+  res := git_libgit2_shutdown();
+  WriteLn(Format('res = %d', [res]));
 end.
